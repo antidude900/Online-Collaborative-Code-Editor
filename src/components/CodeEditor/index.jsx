0b70c0cf4207e293298a2d50d4a7a72c4c8bb56a
Sambal Shrestha <splashofwater900@gmail.com>
2024-12-26 14:00:42 +0545

    fix logo

--- src/components/CodeEditor/index.jsx
+++ src/components/CodeEditor/index.jsx
@@ -56,7 +56,7 @@ export default function CodeEditor() {
 				<div className="editor w-[60vw]">
 					<div className="labels flex items-center mb-5 h-[50px]">
 						<div className="label w-[100px] font-bold ml-3">
-							<img src="../logo.png" width={70} height={10} />
+							<img src="logo.png" width={70} height={10} />
 						</div>
 						<div className="label-buttons flex justify-between grow mr-3">
 							<LanguageMenu language={language} setLanguage={setLanguage} />
