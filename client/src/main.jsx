4bcbf15e1bf3b49db54456025f76d0353c0fa658
Sambal Shrestha <splashofwater900@gmail.com>
2025-01-20 22:00:19 +0545

    Home Added

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,14 +1,35 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./globals.css";
-import App from "./App.jsx";
 import { Provider } from "react-redux";
-import store from './store/store.js'
+import store from "./store/store.js";
+import {
+	createBrowserRouter,
+	Navigate,
+	RouterProvider,
+} from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import Editor from "./pages/Editor.jsx";
+
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <Navigate to="/home" replace={true} />,
+	},
+	{
+		path: "/home",
+		element: <Home />,
+	},
+	{
+		path: "/editor",
+		element: <Editor />,
+	},
+]);
 
 createRoot(document.getElementById("root")).render(
 	<Provider store={store}>
 		<StrictMode>
-			<App />
+			<RouterProvider router={router} />
 		</StrictMode>
 	</Provider>
 );
---
+++ client/src/pages/Editor.jsx
@@ -0,0 +1,12 @@
+import CodeEditor from "../components/CodeEditor";
+
+
+function Editor() {
+    return (
+        <div className="main-container pl-[50px] pr-[50px] pt-10">
+            <CodeEditor />
+        </div>
+    );
+}
+
+export default Editor;
